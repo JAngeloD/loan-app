3fc07091c1f219b806eba6597879d62aeb573997
Entalize <deveraangelo319@gmail.com>
2022-05-26 18:06:55 -0600

    Added borrower detail functionality 50% complete

--- src/renderer/components/AddBorrower.tsx
+++ src/renderer/components/AddBorrower.tsx
@@ -1,10 +1,7 @@
 import React from "react"
 import { fetch } from '../utils/dbaccess'
-import lodash from 'lodash'
 
 export const AddBorrower = () => {
-    
-
     
     return (
         <button>
--- src/renderer/utils/dbaccess.tsx
+++ src/renderer/utils/dbaccess.tsx
@@ -2,26 +2,53 @@ import { join } from 'path'
 import { LowSync, JSONFileSync } from 'lowdb'
 import lodash from 'lodash'
 
+type Date = {
+    month: string
+    paid: number
+    amount: number
+}
+
+type Record = {
+    id: number
+    firstname: string
+    lastname: string
+    loanamount: number
+    frequency: string
+    email: string
+    phone: string
+    totalterms: number
+    interest: number
+    paymentdate: Date[]
+}
+
+type Data = Record[]
+
 class LowWithLodash<T> extends LowSync<T> {
     chain: lodash.ExpChain<this['data']> = lodash.chain(this).get('data')
 }
 
 // Use JSON file for storage
 const filePath = join(__dirname, 'db.json')
-const adapter = new JSONFileSync(filePath)
-const db = new LowSync(adapter)
+const adapter = new JSONFileSync<Data>(filePath)
+const db = new LowWithLodash(adapter)
 
 //Initializes db.data
 db.read();
 
-function fetch(id) {
-    const test = db.data[id];
-    console.log(JSON.stringify(test));
+// Fetches the entire record of the borrower using the ID
+function fetch(id: number, attribute = "none") {
+    var output = db.data[id];
+
+    if (attribute != "none") {
+        output = output[attribute]
+    }
+
+    return output;
 }
 
 function addNewBorrower() {
     // TODO: Write code to add another borrower
-    // PREREQ: Finalize the 
+    // PREREQ: Finalize the JSON structure
     db.write();
 }
 
