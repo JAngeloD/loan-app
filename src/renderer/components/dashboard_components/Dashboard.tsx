4d5a9ec1438c2a7bba1abaaa49cc4600cff629cb
Entalize <deveraangelo319@gmail.com>
2022-05-29 23:37:14 -0600

    Added css for dashboard - only for the 4 header items and not the borrowers list at the bottom

---
+++ src/renderer/components/dashboard_components/Dashboard.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import { BorrowerTable } from '../borrower_components/BorrowerTable';
+import { fetchData } from '../../utils/dbaccess_borrower'
+
+import '../css/dashboard.css';
+
+export const Dashboard = () => {
+
+    return (
+        <div className='dashboard'>
+            <div className="header1">
+                <div className='infoHeader'>
+                    <p className='label'>Money on hand</p>
+                    <p className='amount'>$1000</p>
+                </div>
+            </div>
+            <div className="header2">
+                <div className='infoHeader'>
+                    <p className='label'>Money out for loan</p>
+                    <p className='amount'>$1000</p>
+                </div>
+            </div>
+            <div className="header3">
+                <div className='infoHeader'>
+                    <p className='label'>Revenue</p>
+                    <p className='amount'>$1000</p>
+                </div>
+            </div>
+            <div className="header4">
+                <div className='infoHeader'>
+                    <p className='label'>Expected Revenue</p>
+                    <p className='amount'>$1000</p>
+                </div>
+            </div>
+            <div className="borrowers">
+             </div>
+        </div>
+    );
+}
+
+
