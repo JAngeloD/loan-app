bf71297bce990c71f7b03bf083bbdb327f0bf2e4
Entalize <deveraangelo319@gmail.com>
2022-06-12 17:35:43 -0600

    Removed edit functionality in the detailed borrower section

--- src/renderer/components/borrower_components/PaymentTable.tsx
+++ src/renderer/components/borrower_components/PaymentTable.tsx
@@ -1,7 +1,5 @@
-import React, { useMemo, useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useTable } from 'react-table';
-import { fetchData, editData } from '../../utils/dbaccess_borrower';
-import Popup from 'reactjs-popup';
 
 
 export const PaymentTable = ({ id, getData, data, columns }) => {
@@ -10,9 +8,6 @@ export const PaymentTable = ({ id, getData, data, columns }) => {
         getData(id)
     }, [])
 
-    //State hook for inputting new values to the payment dates of a borrower
-    const [newVal, setNewVal] = useState('')
-
     const {
         getTableProps,
         getTableBodyProps,
@@ -20,15 +15,6 @@ export const PaymentTable = ({ id, getData, data, columns }) => {
         rows,
         prepareRow,
     } = useTable({ columns, data });
-
-    //Style hooks for Pop up elements
-    const contentStyle= {   
-                            background: 'white', 
-                            color: 'black',
-                            margin: '5%'   
-                        };
-    const overlayStyle = { background: 'rgba(0,0,0,0.5)' };
-    const arrowStyle = { color: '#000' }; // style for an svg element
 
     return (
         <>
@@ -49,32 +35,9 @@ export const PaymentTable = ({ id, getData, data, columns }) => {
                             <tr {...row.getRowProps()}>
                                 {row.cells.map((cell) => {
                                     return (
-                                        <Popup
-                                            //Edits the borrower's payment detail on a specific cell
-                                            //cell.row.id = row index
-                                            //id = borrower index (NOT ID, INDEX, THERE IS A DIFFERENCE)
-                                            trigger={
-                                                <td {...cell.getCellProps()}>
-                                                    {cell.render('Cell')}
-                                                </td>
-                                            }
-                                            modal
-                                            {...{contentStyle, overlayStyle, arrowStyle}}
-                                        >
-                                            <div>
-                                                <label>Input new value</label>
-                                                <input onChange={e => {setNewVal(e.target.value)}}/>
-                                                <button onClick={() =>{
-                                                    if(newVal !== '') {
-                                                        editData(id + 1, "payment_dates", newVal, cell.row.id, cell.column.id)
-                                                    }
-                                                    setNewVal('')
-                                                    getData(id)
-                                                }}>
-                                                    Input
-                                                </button>
-                                            </div>
-                                        </Popup>
+                                        <td {...cell.getCellProps()}>
+                                            {cell.render('Cell')}
+                                        </td>
                                     )
                                 })}
                             </tr>
